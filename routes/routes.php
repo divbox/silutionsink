aa73209d0c850471a47535ab3789e903992fceba
John Divney <john.divney@teamaol.com>
2017-01-04 20:13:10 -0500

    new routes

--- routes/routes.php
+++ routes/routes.php
@@ -1,8 +1,26 @@
 <?php
 
 $app->get('/', function($request, $response, $args) {
-  return $this->view->render($response, 'home.twig', [
+  return $this->view->render($response, 'pages/home.twig', [
     'response' => $response
   ]);
-
 });
+
+$app->get('/about', function($request, $response, $args) {
+  return $this->view->render($response, 'pages/about.twig', [
+    'response' => $response
+  ]);
+});
+
+$app->get('/contact', function($request, $response, $args) {
+  return $this->view->render($response, 'pages/contact.twig', [
+    'response' => $response
+  ]);
+});
+
+$app->get('/blog', function($request, $response, $args) {
+  return $this->view->render($response, 'pages/blog.twig', [
+    'response' => $response
+  ]);
+});
+
